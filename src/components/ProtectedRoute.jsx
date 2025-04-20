09173b68c213bb3a7932d9094115bacd62acb755
jugnu prajapati <jugnuprjpt@gmail.com>
2025-04-20 12:53:28 +0530

    build

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -5,7 +5,6 @@ import { useSelector } from "react-redux";
 const ProtectedRoute = () => {
   // Get authentication status from Redux store
   const isAuth = useSelector((state) => state.auth.isAuth);
-
   // If not authenticated, redirect to login page
   if (!isAuth) {
     return <Navigate to="/" replace />;
